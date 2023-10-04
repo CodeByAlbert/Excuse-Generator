11feb7a2ecb8b743ad7e20e1b2f0dd53f85381eb
Albert Rodriguez <rdrgz1992@gmail.com>
2023-10-04 23:20:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Define the generateExcuse function
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["Zombie", "Weatherman", "Mad Scientist", "Necromancer"];
+  let action = ["bit my", "ate my", "destroyed my"];
+  let thing = ["brain", "homework", "Pokemon cards", "playstation"];
+  let where = ["in the basement", "in the cemetery", "in the attic"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let thingIndex = Math.floor(Math.random() * thing.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return String(
+    pronoun[proIndex] +
+      " " +
+      subject[subIndex] +
+      " " +
+      action[actIndex] +
+      " " +
+      thing[thingIndex] +
+      " " +
+      where[whereIndex]
+  );
+};
+
+window.onload = () => {
+  // Add an event listener to the button
+  document.querySelector("#btn").addEventListener("click", () => {
+    let excuse = generateExcuse();
+    document.querySelector("#excuse").textContent = excuse;
+    console.log("Hello"); // Added for debugging purposes
+  });
 };
